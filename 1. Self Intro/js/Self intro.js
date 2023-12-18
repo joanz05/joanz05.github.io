abb78ee17c7f8522dd873954b9e50d3086f9f411
joanz05 <143942789+joanz05@users.noreply.github.com>
2023-12-18 11:34:46 +0800

    Add files via upload

---
+++ 1. Self Intro/js/Self intro.js
@@ -0,0 +1,24 @@
+function scrollToHOME() {
+    var HOMESection = document.getElementById("HOME");
+    HOMESection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToABT() {
+    var ABTSection = document.getElementById("ABT");
+    ABTSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToEdu() {
+    var EduSection = document.getElementById("EDU");
+    EduSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToRes() {
+    var ResSection = document.getElementById("RES");
+    ResSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToCON() {
+    var CONSection = document.getElementById("CON");
+    CONSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
